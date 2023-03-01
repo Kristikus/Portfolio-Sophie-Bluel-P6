33ccac11fec24aff80255561efa3b1515793436e
Kristina Kuster <kriskus@live.fr>
2023-03-01 09:43:41 +0100

    functionnal filters with active buttons

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -30,16 +30,17 @@ const categorieElement = document.querySelector("#portfolio h2");
 const categorieDiv = document.createElement("div");
 categorieElement.after(categorieDiv);
 
-const categorieDivStyle = document.querySelector("#portfolio div");
-categorieDivStyle.classList.add("categories");
+const categorieDivContain = document.querySelector("#portfolio div");
+categorieDivContain.classList.add("categories");
 
 
 // Création bouton "Tous"
 const buttonAll = document.createElement("button");
 buttonAll.innerText = "Tous";
-buttonAll.classList.add("btn", "btn-all");
-categorieDivStyle.appendChild(buttonAll);
+buttonAll.classList.add("btn", "btn_selected");
+categorieDivContain.appendChild(buttonAll);
 
+// Filtre par défaut de tous les travaux au clic sur le bouton "Tous"
 buttonAll.addEventListener("click", function () {
     const buttonAllFilter = works.filter(function (work) {
         return work.categorieId === works.id;
@@ -56,14 +57,27 @@ for (let i = 0; i < categories.length; i++) {
     const buttonFilter = document.createElement('button');
     buttonFilter.classList.add("btn");
     buttonFilter.textContent = categories[i].name;
-    categorieDivStyle.appendChild(buttonFilter);
+    categorieDivContain.appendChild(buttonFilter);
 
+// Filtres par catégories au clic sur les boutons avec une catégorie
     buttonFilter.addEventListener("click", function () {
+
         const worksFiltered = works.filter(function (work) {
             return work.categoryId === works[i].id
         });
         document.querySelector(".gallery").innerHTML = "";
-        worksGen(worksFiltered)
+        worksGen(worksFiltered);
     });
 }
 
+// Boucle sur les boutons pour avoir une classe active sur le bouton cliqué
+const tabButton = categorieDivContain.getElementsByClassName("btn");
+
+for (let i = 0; i < tabButton.length; i++) {
+    tabButton[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("btn_selected");
+        current[0].className = current[0].className.replace(" btn_selected", "");
+        this.className += " btn_selected";
+       
+    });
+}
